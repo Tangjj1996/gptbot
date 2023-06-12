22bfb8f145239394099be47eb5d06c9c902fdb06
Tangjj1996 <awesometangji@gmail.com>
2023-06-12 14:35:43 +0800

    feat: Add self id

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,7 @@
 import { OpenAIApi, Configuration } from "openai";
 import type { MessageInterface } from "wechaty/impls";
-import config from "./config";
+import config from "./config.js";
+import { logger } from "./utils.js";
 
 const configuration = new Configuration({
   apiKey: config.OPENAI_API_KEY,
@@ -9,7 +10,16 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export class ChatGPTBot {
+  /**
+   * message count
+   * don't be rereference
+   */
+  private count = 0;
+
   async onMessage(message: MessageInterface) {
-    await 1;
+    if (message.self() && this.count === 0) {
+      this.count++;
+      await message.say(`debug mode:\n`);
+    }
   }
 }
